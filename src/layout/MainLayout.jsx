6672d0e64b5848f4b14b5258b2e1baf826bdd0b5
Raditya Jagadhita <radityaraditya472@gmail.com>
2026-06-13 23:58:51 +0700

    MainLayout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,9 @@
-import Sidebar from "./Sidebar";
 import Header from "./Header";
 
 export default function MainLayout({ children }) {
   return (
     <div className="layout">
 
-      <Sidebar />
 
       <div className="main-content">
 
